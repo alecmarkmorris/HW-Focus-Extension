071a0cbf260690ea5efa1aecc5c070ea84c23a6f
alecmarkmorris <Alecmarkmorris@gmail.com>
2023-09-11 12:30:34 -0700

    Fixed the start and stop buttons Added functions that starts the work countdown timer

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 //Loads the JavaScript file after the HTML
 document.addEventListener("DOMContentLoaded", function() {
+    
     //Styling the Chrome Extension
     document.body.style.width = "250px";
-    document.body.style.height = "200px";
+    document.body.style.height = "220px";
   
-    //Initializing variables
+    //Initializing HTML variables
     var title = document.getElementById("title");
     var workTime = document.getElementById("workTime");
     var breakTitle = document.getElementById("breakTitle");
     var breakTime = document.getElementById("breakTime");
     var cyclesContainer = document.getElementById("cyclesContainer");
     var btnContainer = document.getElementById("btnContainer");
+    var countDown = document.createElement("p");
+     
+    //Initializing user input variables
+    //Desired work time
+    var workHours = document.getElementById("workHours");
+    var workMinutes = document.getElementById("workMinutes");
+    var workSeconds = document.getElementById("workSeconds");
+    var workTimeInput = [];
+    var workTimeLeft;
+
+    //Desired break time
+    var breakHours = document.getElementById("breakHours");
+    var breakMinutes = document.getElementById("breakMinutes");
+    var breakSeconds = document.getElementById("breakSeconds");
+    var breakTimeInput = [];
 
     //Buttons
     //Start button
     var startBtn = document.getElementById("start");
+
     //Stop 
     var stopBtn = document.createElement("button");
     stopBtn.innerHTML = "Stop";
@@ -42,14 +59,17 @@ document.addEventListener("DOMContentLoaded", function() {
       breakTime.remove();
       cyclesContainer.remove();
       startBtn.remove();
-
+      document.body.appendChild(countDown);
       btnContainer.appendChild(stopBtn);
+      storeValues();
     }
 
-
+    //Funtion that is called when the stop button is pressed
+    //Responsible for displaying the app setting 
     function stop() {
         stopBtn.remove();
         btnContainer.remove();
+        countDown.remove();
         document.body.append(title);
         document.body.append(workTime);
         document.body.append(breakTitle);
@@ -57,6 +77,45 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.append(cyclesContainer);
         document.body.append(btnContainer);
         btnContainer.appendChild(startBtn);
+    }
+
+    //Function that stores all values from user input
+    //Such as Worktime, Breaktime, and Number of Cycles
+    function storeValues() {
+        //Work input
+        workTimeInput = [];
+        workTimeInput.push(workHours.value);
+        workTimeInput.push(workMinutes.value);
+        workTimeInput.push(workSeconds.value);
+
+        //Break input
+        breakTimeInput = [];
+        breakTimeInput.push(breakHours.value);
+        breakTimeInput.push(breakMinutes.value);
+        breakTimeInput.push(breakSeconds.value);
+
+        workTimeLeft = convertTime(workTimeInput[0], workTimeInput[1], workTimeInput[2])
+        startCountdown();
+    }
+
+    //Function that displays the time left for user set worktime
+    function startCountdown() {
+        const interval = setInterval(() => {
+        countDown.innerHTML = "Work Time Left: " +workTimeInput[0]+ ":" +workTimeInput[1]+ ":" + workTimeInput[2] + "Seconds: " + workTimeLeft;
+        workTimeLeft -=1;
+        if (workTimeLeft < 0 ) {
+            clearInterval(interval);
+            countDown.innerHTML = "Time for a break";
+          }
+        }, 1000);
+      }
+
+
+    //Function that takes in hours, minutes, and seconds
+    //and converts them to seconds
+    function convertTime(hours, minutes, seconds) {
+        var timeInSeconds = (hours * 60 * 60) + (minutes * 60) + seconds;
+        return timeInSeconds;
     }
   });
   
